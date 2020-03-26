1aab5da3d820b24b5a2f808e29949046bcc554f2
Michael Meyer <me@entrez.cc>
2020-03-26 15:29:24 -0400

    Add new flags, including expanded help (-h)

--- rcup.py
+++ rcup.py
@@ -4,15 +4,89 @@ import requests
 import os
 import sys
 from hashlib import md5
+import textwrap
 
+# defaults, settings & other preliminary definitions {{{1
+# defaults/settings {{{2
+# Upload to both hardfought and nao by default; behavior can be changed
+# dynamically from the defaults set here with cmdline options
+HARDFOUGHT, NAO = True, True
+# Modify this NH version's rcfile on NAO
 nao_ver = '3.6.6'
+# allowed command line options {{{3
+HDF_FLAGS = ['-hardfought', '-hdf', 'H']
+NAO_FLAGS = ['-nao', '-altorg', 'a']
+HELP_FLAGS = ['-help', 'h', '?']
 
-def print2(*args, **argv):
+
+def negate(flags): # {{{2
+    return ['-no' + f for f in flags if f.startswith('-')]
+
+
+def ffmt(flags): # {{{2
+    return ['-' + f for f in flags]
+
+
+# generate help text congruent with defaults set on line 11 {{{2
+# usage {{{3
+usage = "USAGE: {script} [-h] [-Ha] <USER> <PASS>".format(
+    script = os.path.basename(sys.argv[0])
+)
+# description {{{3
+desc = "This script can update rcfiles on NAO and/or hardfought.org; "\
+    "default behavior is {nao} update NAO and {hdf} update hardfought, "\
+    "but this can be modified by the options listed in the next section."
+# fill in the blanks with current defaults
+desc = desc.format(nao = 'to' if NAO else 'not to',
+                   hdf = 'to' if HARDFOUGHT else 'not to')
+# wrap description to terminal width or 80 columns, whichever is smaller
+try:
+    h, w = os.get_terminal_size()
+    w = 80 if w > 80 else w
+except OSError as ex:
+    w = 80
+desc = textwrap.fill(desc, w)
+# options {{{3
+# use stuff set above (lines 9-18) to generate options list
+options = [
+    [",".join(ffmt(sorted(HELP_FLAGS, key=lambda f:len(f)))),
+     "Display this information"],
+    [",".join(ffmt(sorted(HDF_FLAGS, key=lambda f:len(f)))),
+     "Enable hardfought.org{}".format(' (default)' if HARDFOUGHT else '')],
+    [",".join(ffmt(sorted(negate(HDF_FLAGS), key=lambda f:len(f)))),
+     "Disable hardfought.org{}".format('' if HARDFOUGHT else ' (default)')],
+    [",".join(ffmt(sorted(NAO_FLAGS, key=lambda f:len(f)))),
+     "Enable nethack.alt.org{}".format(' (default)' if NAO else '')],
+    [",".join(ffmt(sorted(negate(NAO_FLAGS), key=lambda f:len(f)))),
+     "Disable nethack.alt.org{}".format('' if NAO else ' (default)')]
+]
+# put it all together {{{3
+HELPTEXT = '{usage}\n\n{description}\n\nOPTIONS:\n{options}\n'.lstrip().format(
+    usage = usage,
+    description = desc,
+    options = '\n\n'.join(
+        ['    {0:<28s}{1:s}'.format(*o) for o in options]
+    )
+)
+
+
+def print2(*args, **argv): # {{{2
     print(*args, **argv, file=sys.stderr)
 
 
-def rcfile_location():
-    # {{{
+def get_home_dir(): # {{{2
+    h = ''
+    if os.name == 'nt':
+        h = os.path.join(
+            os.getenv('HOMEDRIVE', default=os.getenv('HOME')),
+            os.getenv('HOMEPATH', default='')
+        )
+    else:
+        h = os.getenv('HOME')
+    return h
+
+
+def rcfile_location(): # {{{2
     if os.name == 'nt':
         # rcfiles for 3.6.3 and later are saved as
         #   %HOMEDRIVE%\%HOMEPATH%\Nethack\.nethackrc
@@ -21,18 +95,16 @@ def rcfile_location():
         # see https://github.com/NetHack/NetHack/wiki/Windows-NetHack-3.6.3-Breaking-Change-:-Directory-Paths
         rcfl = os.path.join(os.getenv('HOMEDRIVE'), os.getenv('HOMEPATH'), 'Nethack', '.nethackrc')
         if not os.path.isfile(rcfl):
-            rcfl = os.path.join(os.getenv('NETHACKDIR', default=os.getenv('HOME')),
+            rcfl = os.path.join(os.getenv('NETHACKDIR',
+                                          default=get_home_dir()),
                                 'defaults.nh')
     else:
         # on *nix systems just use ~/.nethackrc
         rcfl = os.path.join(os.getenv('HOME'), '.nethackrc')
-
     return rcfl
-    # }}}
 
 
-def server_error(response, explanation = 'connection attempt', **argv):
-    # {{{
+def server_error(response, explanation = 'connection attempt', **argv): # {{{2
     server_name = argv.get('server', 'server')
     print2(
         'error\n{action:s}: {site:s} responded {err:n} {msg:s}'.format(
@@ -42,35 +114,79 @@ def server_error(response, explanation = 'connection attempt', **argv):
             msg = response.reason
         )
     )
-    # }}}
+
+
+def parse_args(args): # {{{2
+    # move flags to their own list {{{3
+    options = []
+    i = 0
+    while i < len(args):
+        a = args[i]
+        if a.startswith('-'):
+            arg = args.pop(i)[1:]
+            options += [arg] if arg.startswith('-') else list(arg)
+        else:
+            i += 1
+    h, n = parse_options(options)
+    # make sure only username & pass remain {{{3
+    if len(args) != 2:
+        print2(usage)
+        exit()
+    u, p = args
+    return (u, p, h, n)
+
+
+def parse_options(options): #{{{2
+    # allowable flags
+    hdf, nao = HARDFOUGHT, NAO
+    for o in options:
+        # enable/disable hdf {{{3
+        if o in HDF_FLAGS:
+            hdf = True
+        elif o in negate(HDF_FLAGS):
+            hdf = False
+        # enable/disable nao {{{3
+        elif o in NAO_FLAGS:
+            nao = True
+        elif o in negate(NAO_FLAGS):
+            nao = False
+        elif o in HELP_FLAGS:
+            print2(HELPTEXT)
+            exit(0)
+        else:
+            option = '-{flag:s}'.format(flag = o)
+            print2('{u}\n\nunrecognized option: {o:s}'.format(u = usage,
+                                                              o = option))
+            exit(0)
+    return (hdf, nao)
+# }}}1
 
 
 args = sys.argv[1:]
-if len(args) != 2:
-    print2('usage: {script} USER PASS'.format(script = os.path.basename(sys.argv[0])))
-    exit()
-
-user, passwd = args
+user, passwd, HARDFOUGHT, NAO = parse_args(args)
 
 rcfile = rcfile_location()
 if not os.path.isfile(rcfile):
-    print2('file does not exist: {}'.format(rcfile))
+    print2('{u}\n\nfile does not exist: {f}'.format(u = usage,
+                                                    f = rcfile))
     exit(1)
 
 try:
     with open(rcfile, 'rb') as f:
         nethackrc = f.read()
 except FileNotFoundError as ex:
-    print2('file does not exist: {}'.format(rcfile))
+    print2('{u}\n\nfile does not exist: {f}'.format(u = usage,
+                                                    f = rcfile))
     exit(1)
 except UnicodeDecodeError as ex:
     # shouldn't happen since we are using rb mode, but leaving it here in case
     # that is changed in the future
-    print2('decode error: {}'.format(ex))
+    print2('{u}\n\ndecode error: {e}'.format(u = usage,
+                                             e = ex))
     exit(1)
 
 if nethackrc is None or len(nethackrc) == 0:
-    print2('error: rcfile is empty')
+    print2('{u}\n\naborting: rcfile is empty'.format(u = usage))
     exit(1)
 else:
     # we need to run the contents of the rcfile through strip() because \n is
@@ -134,6 +250,16 @@ sites = {
         }
     }
 }
+
+if not HARDFOUGHT:
+    removed = sites.pop('hdf')
+
+if not NAO:
+    removed = sites.pop('nao')
+
+if len(sites) == 0:
+    print2('quitting: all sites disabled')
+    exit(0)
 
 for serv, reqs in sites.items():
     print2('updating {servername}'.format(servername = serv), end='...')
