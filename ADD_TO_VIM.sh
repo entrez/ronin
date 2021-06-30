81e38a5263b0f9dfbab4f809186c9d7013885870
Michael Meyer <me@entrez.cc>
2021-06-30 16:58:08 -0400

    Expand functionality of Vim install script
    Allow users to use different usernames and passwords for Hardfought and
    NAO, enable one or the other or both, etc.

--- ADD_TO_VIM.sh
+++ ADD_TO_VIM.sh
@@ -1,24 +1,59 @@
-#!/bin/sh
+#!/bin/bash
+
+vimrcfile="$HOME/.vimrc"
 
 if [ ! -f ./rcup.py ]
 then
     echo >&2 'Please cd into the bundle before running this script.'
     exit 1
-elif [ ! -f ~/.vimrc ]
+elif [ ! -f "$vimrcfile" ]
 then
     echo >&2 'No .vimrc file in your home directory.'
     exit 1
 else
-    echo >&2 'Adds an autocommand to your .vimrc to run rcup.py whenever ~/.nethackrc is'
-    echo >&2 'saved.'
-    echo >&2 'NB: this will write your NAO username & password in plaintext to your .vimrc'
+    echo >&2 'Adds an autocommand to your .vimrc to use "ronin" (via the rcup.py script)'
+    echo >&2 'to sync remote files with your local copy whenever ~/.nethackrc is saved.'
     echo >&2
-    read -p'alt.org username (blank aborts): ' -e nao_user
-    test -n "$nao_user" || exit 0
-    read -p'alt.org password (blank aborts): ' -es nao_pass
-    test -n "$nao_pass" || exit 0
-    echo >>~/.vimrc '" keep nao .nethackrc updated'
-    echo >>~/.vimrc "autocmd! BufWritePost ~/.nethackrc sil!!$PWD/rcup.py $nao_user '$nao_pass' &"
+    echo >&2 'NB: this will write your username & password in plaintext to your .vimrc!!'
+    echo >&2
+    servers=""
+    read -p'alt.org username (blank skips): ' -e nao_user
+    if test -n "$nao_user"; then
+        read -p'alt.org password (blank skips): ' -es nao_pass
+        echo
+        if test -n "$nao_pass"; then
+            servers=" nao"
+        else
+            nao_user=
+        fi
+    fi
+    read -p'hardfought username (blank skips): ' -e hdf_user
+    if test -n "$hdf_user"; then
+        read -p'hardfought password (blank skips): ' -es hdf_pass
+        echo
+        if test -n "$hdf_pass"; then
+            servers="$servers$([ -n "$servers" ] && printf " and " || printf " ")hdf"
+        else
+            hdf_user=
+        fi
+    fi
+    if [ -z "$servers" ]; then
+        echo >&2 "no servers selected. goodbye"
+        exit 0
+    fi
+    printf >>"$vimrcfile" '" keep .nethackrc updated on%s via ronin\n' "$servers"
+    autocmd=""
+    if [ "$hdf_user" = "$nao_user" ] && [ "$hdf_pass" = "$nao_pass" ]; then
+        autocmd="$PWD/rcup.py -ha $nao_user '$nao_pass' &"
+    else
+        if [ -n "$nao_user" ]; then
+            autocmd="$PWD/rcup.py  --nao --no-hdf $nao_user '$nao_pass' &"
+        fi
+        if [ -n "$hdf_user" ]; then
+            autocmd="${autocmd}$([ -n "$autocmd" ] && echo "; ")$PWD/rcup.py --no-nao --hdf $hdf_user '$hdf_pass' &"
+        fi
+    fi
+    echo >>"$vimrcfile" "autocmd! BufWritePost ~/.nethackrc sil!!$autocmd"
     echo >&2
     echo >&2 'Done.'
 fi
